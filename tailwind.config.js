dec2d1d4b73ea236bcf6810f295cfe3eac1a70fa
SonKkc <soncb.xyz@gmail.com>
2025-04-28 10:18:19 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+export default {
+  content: ["*"],
+  theme: {
+    screens: {
+    },
+    extend: {
+      colors: {
+      },
+    },
+  },
+  plugins: [],
+}
+
