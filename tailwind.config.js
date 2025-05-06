953d66fd8d01aabeaf9f7b0f581a17c1a3aed762
SonKkc <soncb.xyz@gmail.com>
2025-05-06 16:27:14 +0700

    update 6/5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
   content: ["*"],
   theme: {
-    screens: {
-    },
     extend: {
       colors: {
+        "brand-orange": "#FF9900",
+        "dark-gray": "#121212",
+        "darker-gray": "#0A0A0A",
       },
     },
   },
-  plugins: [],
-}
-
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
+};
